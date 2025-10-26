0770fdd58d2fe0b1c83c0b31c64b8428ca029379
Óscar Míguez Alonso <omiguezalonso@gmail.com>
2025-10-26 18:33:36 +0100

    Finished adapting the sample plugin to the new purpose

--- EnergySavingAltTab/Configuration.cs
+++ EnergySavingAltTab/Configuration.cs
@@ -1,17 +1,13 @@
-﻿using Dalamud.Configuration;
+using Dalamud.Configuration;
 using System;
 
-namespace SamplePlugin;
+namespace EnergySavingAltTab;
 
 [Serializable]
 public class Configuration : IPluginConfiguration
 {
     public int Version { get; set; } = 0;
 
-    public bool IsConfigWindowMovable { get; set; } = true;
-    public bool SomePropertyToBeSavedAndWithADefault { get; set; } = true;
-
-    // The below exist just to make saving less cumbersome
     public void Save()
     {
         Plugin.PluginInterface.SavePluginConfig(this);
